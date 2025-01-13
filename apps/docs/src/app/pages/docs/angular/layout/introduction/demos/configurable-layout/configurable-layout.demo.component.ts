b51a85159aeee8fbd1b4c1b837a7d9a9ab0f0b16
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2025-01-13 10:30:58 +0100

    documentation(ngx-layout): Update introduction page

---
+++ apps/docs/src/app/pages/docs/angular/layout/introduction/demos/configurable-layout/configurable-layout.demo.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+	selector: 'ngx-configurable-layout-demo',
+	styleUrl: './configurable-layout.demo.component.scss',
+	templateUrl: './configurable-layout.demo.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NgxConfigurableLayoutDemoComponent {}
---
+++ apps/docs/src/app/pages/docs/angular/layout/introduction/demos/data.demo.ts
@@ -0,0 +1,44 @@
+export const NgxAccordionDemoData = [
+	{
+		name: 'Iola Ware',
+		phone: '(284) 962-5266',
+		email: 'felis.purus.ac@yahoo.ca',
+		country: 'Philippines',
+	},
+	{
+		name: 'Risa Petersen',
+		phone: '(866) 576-3226',
+		email: 'lacinia.vitae.sodales@protonmail.com',
+		country: 'Norway',
+	},
+	{
+		name: 'Geraldine Berger',
+		phone: '1-626-948-7981',
+		email: 'quam.pellentesque.habitant@yahoo.org',
+		country: 'Philippines',
+	},
+	{
+		name: 'Aquila Bonner',
+		phone: '(333) 239-4766',
+		email: 'orci.lobortis@google.com',
+		country: 'Norway',
+	},
+	{
+		name: 'Yolanda Little',
+		phone: '(605) 573-9355',
+		email: 'libero.donec@google.couk',
+		country: 'Sweden',
+	},
+];
+
+export const NgxConfigurableLayoutDemoData = {
+	userAmount: 100,
+	onlineUsers: 67,
+	unreadMessages: 10,
+	goals: {
+		a: 50,
+		b: 88,
+		c: 40,
+		d: 100,
+	},
+};
