f9f256f23b7555ff0d6113dbbeda5a1f5f77af00
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2025-06-02 08:58:44 +0200

    feat(ngx-gov-flanders): Add documentation for ngx-gov-flanders

---
+++ apps/docs/src/app/categories/angular/government/sub-categories/flanders/ng-doc.category.ts
@@ -0,0 +1,10 @@
+import { NgDocCategory } from '@ng-doc/core';
+import GovernmentCategory from '../../ng-doc.category';
+
+const FlemishGovernmentCategory: NgDocCategory = {
+	title: 'Flanders',
+	category: GovernmentCategory,
+	order: 2,
+};
+
+export default FlemishGovernmentCategory;
---
+++ apps/docs/src/app/pages/docs/angular/government/flanders/installation/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import { FlemishGovernmentCategory } from '../../../../../../categories/angular';
+
+const InstallationPage: NgDocPage = {
+	title: `Installation`,
+	mdFile: './index.md',
+	category: FlemishGovernmentCategory,
+	order: 1,
+};
+
+export default InstallationPage;
