3287efbb92e6db63eefceab8781772760ec1785a
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2025-07-08 08:25:33 +0200

    feat(ngx-cookies): Export needed types

--- libs/angular/cookies/src/index.ts
+++ libs/angular/cookies/src/index.ts
@@ -5,3 +5,8 @@ export { NgxCookieService } from './lib/services';
 export { NgxHasCookieDirective } from './lib/directives';
 export { NgxCookiesFallBackComponent } from './lib/abstracts';
 export { NgxCookiesFallbackComponentToken } from './lib/tokens';
+export {
+	NgxCookieConfiguration,
+	NgxCookieCategories,
+	NgxCookieLanguageConfiguration,
+} from './lib/types';
